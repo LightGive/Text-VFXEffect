3913bdf77f23a4793e53bfd72491cca0bca7ef6a
LightGive <roi.akase@outlook.jp>
2020-04-29 14:32:04 +0900

    フォント追加など

---
+++ TextEffect/Assets/Scripts/FontTexture.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FontTexture : MonoBehaviour
+{  
+    [SerializeField]
+    private Font  testFont;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
